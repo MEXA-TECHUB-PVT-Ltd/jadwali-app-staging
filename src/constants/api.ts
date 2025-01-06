57810f427a2a37081e41aa1b3f5385d5b102ea6c
developer1Mtechub <muhammadfaisal@mtechub.org>
2025-01-06 14:44:44 +0500

    run project and remove pricing from add event

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,3 +2,4 @@
 export const API_URL = 'https://jadwali-be.caprover-demo.mtechub.com/'; //new...when given to irfan
 export const Web_URL = `https://jadwali-webview.netlify.app/free-schedule/`;
 // export const API_URL = 'http://192.168.18.225/';
+// export const API_URL = 'http://192.168.18.24:3003/';
